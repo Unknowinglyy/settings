8d9f0fcc45cfa70a0c68c2b26c79d5a9095a9fc7
Unknowinglyy <unknowingly@tamu.edu>
2026-02-13 14:36:59 -0600

    blink
    blink might be the goat. also why does clangd have function signature
    snippets by default...

--- nvim/lua/blake/plugins/lsp/lspconfig.lua
+++ nvim/lua/blake/plugins/lsp/lspconfig.lua
@@ -1,179 +1,189 @@
 return {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        -- "saghen/blink.cmp",
-        { "antosha417/nvim-lsp-file-operations", config = true },
-    },
-    config = function()
-        -- NOTE: LSP Keybinds
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		-- "hrsh7th/cmp-nvim-lsp",
+		"saghen/blink.cmp",
+		{ "antosha417/nvim-lsp-file-operations", config = true },
+	},
+	config = function()
+		-- NOTE: LSP Keybinds
 
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-            callback = function(ev)
-                -- Buffer local mappings
-                -- Check `:help vim.lsp.*` for documentation on any of the below functions
-                local opts = { buffer = ev.buf, silent = true }
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				-- Buffer local mappings
+				-- Check `:help vim.lsp.*` for documentation on any of the below functions
+				local opts = { buffer = ev.buf, silent = true }
 
-                -- keymaps
-                opts.desc = "Show LSP references"
-                vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+				-- keymaps
+				opts.desc = "Show LSP references"
+				vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
-                opts.desc = "Go to declaration"
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+				opts.desc = "Go to declaration"
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
-                opts.desc = "Show LSP definitions"
-                vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+				opts.desc = "Show LSP definitions"
+				vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
 
-                opts.desc = "Show LSP implementations"
-                vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+				opts.desc = "Show LSP implementations"
+				vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
-                opts.desc = "Show LSP type definitions"
-                vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+				opts.desc = "Show LSP type definitions"
+				vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-                opts.desc = "See available code actions"
-                vim.keymap.set({ "n", "v" }, "<leader>vca", function() vim.lsp.buf.code_action() end, opts) -- see available code actions, in visual mode will apply to selection
+				opts.desc = "See available code actions"
+				vim.keymap.set({ "n", "v" }, "<leader>ca", function()
+					vim.lsp.buf.code_action()
+				end, opts) -- see available code actions, in visual mode will apply to selection
 
-                opts.desc = "Smart rename"
-                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+				opts.desc = "Smart rename"
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
-                opts.desc = "Show buffer diagnostics"
-                vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+				opts.desc = "Show buffer diagnostics"
+				vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
-                opts.desc = "Show line diagnostics"
-                vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+				opts.desc = "Show line diagnostics"
+				vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-                opts.desc = "Show documentation for what is under cursor"
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+				opts.desc = "Show documentation for what is under cursor"
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-                opts.desc = "Restart LSP"
-                vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+				opts.desc = "Restart LSP"
+				vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 
-                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-            end,
-        })
+				opts.desc = "Show signature while in insert mode"
+				vim.keymap.set("i", "<C-h>", function()
+					vim.lsp.buf.signature_help()
+				end, opts)
+			end,
+		})
 
+		-- NOTE : Moved all this to Mason including local variables
+		-- used to enable autocompletion (assign to every lsp server config)
+		-- local capabilities = cmp_nvim_lsp.default_capabilities()
+		-- Change the Diagnostic symbols in the sign column (gutter)
 
-        -- NOTE : Moved all this to Mason including local variables
-        -- used to enable autocompletion (assign to every lsp server config)
-        -- local capabilities = cmp_nvim_lsp.default_capabilities()
-        -- Change the Diagnostic symbols in the sign column (gutter)
+		-- Define sign icons for each severity
+		local signs = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.HINT] = "󰠠 ",
+			[vim.diagnostic.severity.INFO] = " ",
+		}
 
-        -- Define sign icons for each severity
-        local signs = {
-            [vim.diagnostic.severity.ERROR] = " ",
-            [vim.diagnostic.severity.WARN]  = " ",
-            [vim.diagnostic.severity.HINT]  = "󰠠 ",
-            [vim.diagnostic.severity.INFO]  = " ",
-        }
+		-- Set the diagnostic config with all icons
+		vim.diagnostic.config({
+			signs = {
+				text = signs, -- Enable signs in the gutter
+			},
+			virtual_text = true, -- Specify Enable virtual text for diagnostics
+			underline = true, -- Specify Underline diagnostics
+			update_in_insert = false, -- Keep diagnostics active in insert mode
+		})
 
-        -- Set the diagnostic config with all icons
-        vim.diagnostic.config({
-            signs = {
-                text = signs -- Enable signs in the gutter
-            },
-            virtual_text = true,  -- Specify Enable virtual text for diagnostics
-            underline = true,     -- Specify Underline diagnostics
-            update_in_insert = false,  -- Keep diagnostics active in insert mode
-        })
+		-- NOTE :
+		-- Moved back from mason_lspconfig.setup_handlers from mason.lua file
+		-- as mason setup_handlers is deprecated & its causing issues with lsp settings
+		--
+		-- Setup servers
+		-- not needed anymore with blink... hopefully...
+		-- local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		-- local capabilities = cmp_nvim_lsp.default_capabilities()
+		--
+		-- -- global LSP settings (applied to all)
+		-- vim.lsp.config("*", {
+		--   capabilities = capabilities,
+		-- })
 
+		-- Config and enable lsp servers here
+		-- lua_ls
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+					completion = {
+						callSnippet = "Disable",
+					},
+					workspace = {
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.stdpath("config") .. "/lua"] = true,
+						},
+					},
+				},
+			},
+		})
+		vim.lsp.enable("lua_ls")
 
-        -- NOTE :
-        -- Moved back from mason_lspconfig.setup_handlers from mason.lua file
-        -- as mason setup_handlers is deprecated & its causing issues with lsp settings
-        --
-        -- Setup servers
-        local cmp_nvim_lsp = require("cmp_nvim_lsp")
-        local capabilities = cmp_nvim_lsp.default_capabilities()
+		--C & C++
+		vim.lsp.config("clangd", {
+			cmd = {
+				"clangd",
+				"--function-arg-placeholders=false",
+			},
+		})
+		vim.lsp.enable("clangd")
 
-        -- global LSP settings (applied to all)
-        vim.lsp.config('*', {
-            capabilities = capabilities,
-        })
+		-- Python
+		-- patch to handle error "change_annotations must be provided for annotated text edits" for pyright
+		-- source of error from https://github.com/neovim/neovim/issues/34731#issuecomment-3169771547
 
-        -- Config and enable lsp servers here
-        -- lua_ls
-        vim.lsp.config("lua_ls", {
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
-                    completion = {
-                        callSnippet = "Replace",
-                    },
-                    workspace = {
-                        library = {
-                            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                            [vim.fn.stdpath("config") .. "/lua"] = true,
-                        },
-                    },
-                },
-            },
-        })
-        vim.lsp.enable("lua_ls")
+		-- "Pyright is being non-compliant here by returning `annotationId` in the edits, but not
+		-- populating the `changeAnnotations` field in the `WorkspaceEdit`. This causes Neovim to
+		-- throw an error when applying the workspace edit."
+		vim.lsp.config("pyright", {
+			handlers = {
+				[vim.lsp.protocol.Methods.textDocument_rename] = function(err, result, cntx)
+					if err then
+						vim.notify("Pyright rename failed: " .. err.message, vim.log.levels.ERROR)
+						return
+					end
 
-        --C & C++
-        vim.lsp.enable("clangd")
+					--@cast result lsp.WorkspaceEdit
+					for _, change in ipairs(result.documentChanges or {}) do
+						for _, edit in ipairs(change.edits or {}) do
+							if edit.annotationId then
+								edit.annotationId = nil
+							end
+						end
+					end
 
-        -- Python
-        -- patch to handle error "change_annotations must be provided for annotated text edits" for pyright
-        -- source of error from https://github.com/neovim/neovim/issues/34731#issuecomment-3169771547
+					local client = assert(vim.lsp.get_client_by_id(cntx.client_id))
+					vim.lsp.util.apply_workspace_edit(result, client.offset_encoding)
+				end,
+			},
+		})
+		vim.lsp.enable("pyright")
 
-        -- "Pyright is being non-compliant here by returning `annotationId` in the edits, but not
-        -- populating the `changeAnnotations` field in the `WorkspaceEdit`. This causes Neovim to
-        -- throw an error when applying the workspace edit."
-        vim.lsp.config("pyright", {
-            handlers = {
-                [vim.lsp.protocol.Methods.textDocument_rename] = function(err, result, cntx)
-                    if err then
-                        vim.notify('Pyright rename failed: ' .. err.message, vim.log.levels.ERROR)
-                        return
-                    end
+		--Go
+		vim.lsp.config("gopls", {
+			settings = {
+				gopls = {
+					analyses = {
+						unusedparams = true,
+					},
+					staticcheck = true,
+					gofumpt = true, --go formatter
+				},
+			},
+		})
+		vim.lsp.enable("gopls")
 
-                --@cast result lsp.WorkspaceEdit
-                for _, change in ipairs(result.documentChanges or {}) do
-                    for _, edit in ipairs(change.edits or {}) do
-                        if edit.annotationId then
-                            edit.annotationId = nil
-                        end
-                    end
-                end
-
-                local client = assert(vim.lsp.get_client_by_id(cntx.client_id))
-                vim.lsp.util.apply_workspace_edit(result, client.offset_encoding)
-                end,
-            },
-        })
-        vim.lsp.enable("pyright")
-
-        --Go
-        vim.lsp.config("gopls", {
-            settings = {
-                gopls = {
-                    analyses = {
-                        unusedparams = true,
-                    },
-                    staticcheck = true,
-                    gofumpt = true, --go formatter
-                },
-            },
-        })
-        vim.lsp.enable("gopls")
-
-        vim.lsp.config("rust_analyzer", {
-            settings = {
-                ["rust-analyzer"] = {
-                    cargo = {
-                        allFeatures = true,
-                    },
-                    checkOnSave = {
-                        command = "clippy",
-                    },
-                },
-            },
-        })
-        vim.lsp.enable("rust_analyzur")
-    end,
+		vim.lsp.config("rust_analyzer", {
+			settings = {
+				["rust-analyzer"] = {
+					cargo = {
+						allFeatures = true,
+					},
+					checkOnSave = {
+						command = "clippy",
+					},
+				},
+			},
+		})
+		vim.lsp.enable("rust_analyzer")
+	end,
 }
